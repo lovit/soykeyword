aadfbe45fed96d01a6399a87a5ee18f0d5025b82
lovit <soy.lovit@gmail.com>
2017-05-22 21:21:53 +0900

    Add requires; soynlp

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
    description="Unsupervised Korean Natural Language Processing Toolkits",
    long_description="""It contains two keyword extraction algorithms. First one uses Lasso logistic regression and the other uses relative proportion ratio
    """,
-   install_requires=["numpy", "scikit-learn"],
+   install_requires=["numpy", "scikit-learn", "soynlp>=0.0.17"],
    keywords = ['keyword extractor'],
    packages=['soykeyword']
 )
