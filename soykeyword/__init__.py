6c2d44e152b65983f7b584eecb174f82bcb04386
lovit <soy.lovit@gmail.com>
2017-05-22 21:22:06 +0900

    Commit get_memory_usage, DoublespaceLineCorpus

--- soykeyword/__init__.py
+++ soykeyword/__init__.py
@@ -6,3 +6,6 @@ __copyright__ = 'Copyright 2017 Lovit'
 
 from . import lasso
 from . import proportion
+from .utils import get_available_memory
+from .utils import get_process_memory
+from .utils import DoublespaceLineCorpus
